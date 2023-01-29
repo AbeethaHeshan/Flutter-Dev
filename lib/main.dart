35da2930d5d5f61eee704a5a4d5a0b9b27a75844
AbeethaHeshan <abeethaheshan400@gmail.com>
2023-01-29 13:14:52 +0530

    new branch

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'dart:ffi';
+import 'dart:math';
+
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: Scaffold(
+      appBar:AppBar(
+         title:const Center(
+               child: Text("HI")
+           )
+      ),
+      body:
+       const Center(
+          child: Text("Hello !"),
+       ),
+    ),
+  ));
+}
+
+
+
+
