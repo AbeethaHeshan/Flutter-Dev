5936d276684a9e802d0b01db123fad22cc05bfa0
AbeethaHeshan <abeethaheshan400@gmail.com>
2023-02-02 22:47:20 +0530

    login Screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,86 @@
-import 'dart:ffi';
-import 'dart:math';
-
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-// create four boxes in four corners and the each  boxes names are 1,2,3,4 the name should be center
+
 void main() {
-  runApp(MaterialApp(
-    home: Scaffold(
-      appBar:AppBar(
-         title:const Text(" Widgets Positioning")
-      ),
-      body:
-         const Center(
-           child: Image(
-             image: AssetImage("assets/images/Car.png"),
+  runApp( MaterialApp(
+
+      home: Scaffold(
+         backgroundColor: Colors.indigoAccent,
+         body: Center(
+           child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                   const CircleAvatar(
+                       radius: 40,
+                       backgroundImage: ExactAssetImage("assets/images/Avatar.png"),
+                   ),
+                  const Text("Abeetha Heshan",
+                    style: TextStyle(
+                    fontFamily: 'Pacifico',
+                    color: Colors.white,
+                    fontSize: 35,
+                  ),),
+                   const Text("Flutter Developer",
+                      style: TextStyle(
+                       // fontFamily: 'Your Font Family',
+                        fontSize: 18,
+                        color: Colors.white,
+                      ),
+                    ),
+                   Container(
+                     margin: const EdgeInsets.symmetric(vertical: 10),
+                     width: 100,
+                     decoration: const BoxDecoration(
+                       border: Border(
+                         bottom: BorderSide(
+                           color: Colors.white,
+                           width: 1,
+                         ),
+                       ),
+                     ),
+
+
+                   ),
+                   Padding(
+                     padding: const EdgeInsets.all(8.0),
+                     child: SizedBox(
+                       width:350,
+                       child: TextField(
+                          decoration: InputDecoration(
+                            border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(8),
+                             borderSide:BorderSide.none,
+                            ),
+                            filled: true,
+                            fillColor: Colors.white,
+                            prefixIcon: const Icon(Icons.call, color: Colors.indigoAccent),
+                            hintText: ' +94111111111',
+                          ),
+                          ),
+                     ),
+                   ),
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: SizedBox(
+                        width:350,
+                        child: TextField(
+                          decoration: InputDecoration(
+                            border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(8),
+                              borderSide:BorderSide.none,
+                            ),
+                            filled: true,
+                            fillColor: Colors.white,
+                            prefixIcon: const Icon(Icons.mail, color: Colors.indigoAccent),
+                            hintText: ' abcdefg@gmail.com',
+                          ),
+                        ),
+                      ),
+                    ),
+
+              ],
            ),
-         )
-    ),
+         ),
+      ),
   ));
 }
 
