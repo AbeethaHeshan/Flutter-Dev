69b75d2ef93f7eeea4a103ad9f7e06164e6f5da9
AbeethaHeshan <abeethaheshan400@gmail.com>
2023-02-03 14:42:08 +0530

    login card

--- lib/main.dart
+++ lib/main.dart
@@ -11,10 +11,67 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return  MaterialApp(
       home: Scaffold(
-          backgroundColor: Colors.red,
-          body: Center(child: Text("StateLessWidget")),
+          backgroundColor: Colors.amber[500],
+          body:SafeArea(
+              child: Center(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                     children:  [
+                       const CircleAvatar(
+                         radius: 40,
+                          backgroundImage: AssetImage("assets/images/Avatar.png"),
+                       ),
+                       const Text("Abeetha Hesahan",style: TextStyle(fontFamily: "Pacifico",fontSize: 30,color: Colors.white),),
+                       const Text("SOFTWARE DEVELOPER",style: TextStyle(fontSize:15,color: Colors.white)),
+                       Padding(
+                         padding: const EdgeInsets.all(8.0),
+                         child: Container(
+                           width: 250,
+                           height: 40,
+                           color: Colors.white,
+                           child: Row(
+                              children: const [
+                                 Padding(
+                                   padding: EdgeInsets.only(left: 10),
+                                   child: Icon(Icons.call,color: Colors.amber,),
+                                 ),
+                                 Padding(
+                                   padding: EdgeInsets.only(left: 10),
+                                   child: Text("+947789456",style: TextStyle(color: Colors.amber)),
+                                 )
+                              ],
+
+                           ),
+                         ),
+                       ),
+                       Padding(
+                         padding: const EdgeInsets.all(8.0),
+                         child: Container(
+                           width: 250,
+                           height: 40,
+                           color: Colors.white,
+                           child: Row(
+                             children: const [
+                               Padding(
+                                 padding: EdgeInsets.only(left: 10),
+                                 child: Icon(Icons.mail,color: Colors.amber,),
+                               ),
+                               Padding(
+                                 padding: EdgeInsets.only(left: 10),
+                                 child: Text("abeethaheshan@xyz.com",style: TextStyle(color:  Colors.amber),),
+                               )
+                             ],
+
+                           ),
+                         ),
+                       )
+
+                     ],
+                ),
+              )
+          ),
       ),
     );
   }
