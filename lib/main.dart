dc1f40d52b12132366d2f80eb2d9a5c4338678df
AbeethaHeshan <abeethaheshan400@gmail.com>
2023-02-10 18:50:31 +0530

    Dice

--- lib/main.dart
+++ lib/main.dart
@@ -1,81 +1,48 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(  MaterialApp(
+     home: Scaffold(
+       backgroundColor: Colors.red,
+        appBar:AppBar(
+           title: const Center(child: Text("Dicee")),
+           backgroundColor: Colors.red,
+        ),
+        body: const SafeArea(child: DicePage()),
+     ),
+  ));
 }
-//state less && state full  supports hot reload
-//hot reload does not effect for the state
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+
+class DicePage extends StatelessWidget {
+  const DicePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return  MaterialApp(
-      home: Scaffold(
-          backgroundColor: Colors.amber[500],
-          body:SafeArea(
-              child: Center(
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                     children:  [
-                       const CircleAvatar(
-                         radius: 40,
-                          backgroundImage: AssetImage("assets/images/Avatar.png"),
-                       ),
-                       const Text("Abeetha Hesahan",style: TextStyle(fontFamily: "Pacifico",fontSize: 30,color: Colors.white),),
-                       const Text("SOFTWARE DEVELOPER",style: TextStyle(fontSize:15,color: Colors.white)),
-                       Padding(
-                         padding: const EdgeInsets.all(8.0),
-                         child: Container(
-                           width: 250,
-                           height: 40,
-                           color: Colors.white,
-                           child: Row(
-                              children: const [
-                                 Padding(
-                                   padding: EdgeInsets.only(left: 10),
-                                   child: Icon(Icons.call,color: Colors.amber,),
-                                 ),
-                                 Padding(
-                                   padding: EdgeInsets.only(left: 10),
-                                   child: Text("+947789456",style: TextStyle(color: Colors.amber)),
-                                 )
-                              ],
-
-                           ),
-                         ),
-                       ),
-                       Padding(
-                         padding: const EdgeInsets.all(8.0),
-                         child: Container(
-                           width: 250,
-                           height: 40,
-                           color: Colors.white,
-                           child: Row(
-                             children: const [
-                               Padding(
-                                 padding: EdgeInsets.only(left: 10),
-                                 child: Icon(Icons.mail,color: Colors.amber,),
-                               ),
-                               Padding(
-                                 padding: EdgeInsets.only(left: 10),
-                                 child: Text("abeethaheshan@xyz.com",style: TextStyle(color:  Colors.amber),),
-                               )
-                             ],
-
-                           ),
-                         ),
-                       )
-
-                     ],
-                ),
-              )
-          ),
+    return Center(
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Row(
+          children: [
+             Expanded(
+                 flex:2,
+                 child:TextButton(
+                     onPressed: () {  },
+                     child: Image.asset('assets/images/dice/dice3.png'))),
+              Expanded(
+                 flex:2,
+                 child:TextButton(
+                     onPressed: () {  },
+                     child: Image.asset('assets/images/dice/dice2.png')))
+          ],
+        ),
       ),
     );
   }
 }
+
+
+
 
 
 
